a1e6d06c096e6587b487dbe8cd6bf67000deed45
Stilyana K. Stoyanova <skstoqnova@yahoo.com>
2022-08-16 01:27:58 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        brightRed: "#13b3c7",
+        brightRedLight: "#5ccedb",
+        brightRedSupLight: "#c9f9ff",
+        darkBlue: "hsl(228, 39%, 23%)",
+        darkGrayishBlue: "hsl(227, 12%, 61%)",
+        veryDarkBlue: "hsl(233, 12%, 13%)",
+        veryPaleRed: "hsl(13, 100%, 96%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+      },
+    },
+  },
+  plugins: [],
+}
